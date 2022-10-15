0583c0fcba3398aa45e4300cb5f8c5b896930be2
Inshaullah <73126487+MianInshaullah@users.noreply.github.com>
2022-10-16 01:50:49 +0500

    Create CountingSort.py

---
+++ python/CountingSort.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countSort' function below.
+#
+# The function accepts 2D_STRING_ARRAY arr as parameter.
+#
+def countSort(arr):
+    for i in range(len(arr) // 2):
+        arr[i][1] = '-'
+    arr.sort(key=lambda x: int(x[0]))
+    print(' '.join(x[1] for x in arr))
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(input().rstrip().split())
+
+    countSort(arr)
